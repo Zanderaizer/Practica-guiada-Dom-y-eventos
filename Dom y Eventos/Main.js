d4638a0f0e79285ec42ffbf1a62198e7eb00738d
Zanderaizer <160615953+Zanderaizer@users.noreply.github.com>
2025-03-24 10:45:10 -0400

    Add files via upload

---
+++ Dom y Eventos/Main.js
@@ -0,0 +1,12 @@
+const parrafo = document.getElementById('miParrafo');
+        const boton = document.getElementById('cambiarColorBtn');
+
+      
+        function cambiarColor() {
+       
+            const colorAleatorio = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            parrafo.style.color = colorAleatorio; 
+        }
+
+        
+        boton.addEventListener('click', cambiarColor);
